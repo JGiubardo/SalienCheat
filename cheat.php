372402059aa11dbb38c7e8890e036d00b6ae32f9
JGiubardo <38842395+JGiubardo@users.noreply.github.com>
2018-06-22 10:48:15 -0400

    Planet selection update
    Updated from base, with additional messages, and planet selection.

--- cheat.php
+++ cheat.php
@@ -1,35 +1,25 @@
 <?php
-
 set_time_limit( 0 );
-
 $Token = trim( file_get_contents( __DIR__ . '/token.txt' ) );
-
 SendPOST( 'ITerritoryControlMinigameService/RepresentClan', 'clanid=33035916&access_token=' . $Token );
-
 lol_using_goto_in_2018:
-
 do
 {
 	$CurrentPlanet = GetFirstAvailablePlanet();
 }
 while( !$CurrentPlanet && sleep( 5 ) === 0 );
-
 SendPOST( 'ITerritoryControlMinigameService/JoinPlanet', 'id=' . $CurrentPlanet . '&access_token=' . $Token );
-
 do
 {
 	$Data = SendPOST( 'ITerritoryControlMinigameService/GetPlayerInfo', 'access_token=' . $Token );
 }
 while( !isset( $Data[ 'response' ][ 'active_planet' ] ) );
-
 // Set the planet to what Steam thinks is the active one, even though we sent JoinPlanet request
 $CurrentPlanet = $Data[ 'response' ][ 'active_planet' ];
-
 if( isset( $Data[ 'response' ][ 'active_zone_game' ] ) )
 {
 	SendPOST( 'IMiniGameService/LeaveGame', 'access_token=' . $Token . '&gameid=' . $Data[ 'response' ][ 'active_zone_game' ] );
 }
-
 do
 {
 	do
@@ -37,18 +27,13 @@ do
 		$Zone = GetFirstAvailableZone( $CurrentPlanet );
 	}
 	while( !$Zone && sleep( 5 ) === 0 );
-
 	$Zone = SendPOST( 'ITerritoryControlMinigameService/JoinZone', 'zone_position=' . $Zone[ 'zone_position' ] . '&access_token=' . $Token );
-
 	if( empty( $Zone[ 'response' ][ 'zone_info' ] ) )
 	{
 		Msg( 'Failed to join a zone, waiting 15 seconds and trying again' );
-
 		sleep( 15 );
-
 		goto lol_using_goto_in_2018;
 	}
-
 	$Zone = $Zone[ 'response' ][ 'zone_info' ];
 	
 	Msg( 'Joined zone ' . $Zone[ 'zone_position' ] . ' - Captured: ' . number_format( $Zone[ 'capture_progress' ] * 100, 2 ) . '% - Difficulty: ' . $Zone[ 'difficulty' ] );
@@ -56,16 +41,13 @@ do
 	sleep( 120 );
 	
 	$Data = SendPOST( 'ITerritoryControlMinigameService/ReportScore', 'access_token=' . $Token . '&score=' . GetScoreForZone( $Zone ) . '&language=english' );
-
 	if( isset( $Data[ 'response' ][ 'new_score' ] ) )
 	{
 		$Data = $Data[ 'response' ];
-
 		Msg( 'Score: ' . $Data[ 'old_score' ] . ' => ' . $Data[ 'new_score' ] . ' (next level: ' . $Data[ 'next_level_score' ] . ') - Current level: ' . $Data[ 'new_level' ] );
 	}
 }
 while( true );
-
 function GetScoreForZone( $Zone )
 {
 	switch( $Zone[ 'difficulty' ] )
@@ -77,16 +59,13 @@ function GetScoreForZone( $Zone )
 	
 	return $Score * 120;
 }
-
 function GetFirstAvailableZone( $Planet )
 {
 	$Zones = SendGET( 'GetPlanet', 'id=' . $Planet );
-
 	if( empty( $Zones[ 'response' ][ 'planets' ][ 0 ][ 'zones' ] ) )
 	{
 		return null;
 	}
-
 	$Zones = $Zones[ 'response' ][ 'planets' ][ 0 ][ 'zones' ];
 	$CleanZones = [];
 	
@@ -102,7 +81,6 @@ function GetFirstAvailableZone( $Planet )
 	{
 		return null;
 	}
-
 	usort( $CleanZones, function( $a, $b )
 	{
 		if( $b[ 'difficulty' ] === $a[ 'difficulty' ] )
@@ -112,34 +90,37 @@ function GetFirstAvailableZone( $Planet )
 		
 		return $b[ 'difficulty' ] - $a[ 'difficulty' ];
 	} );
-
 	return $CleanZones[ 0 ];
 }
-
 function GetFirstAvailablePlanet()
 {
 	$Planets = SendGET( 'GetPlanets', 'active_only=1' );
-
 	if( empty( $Planets[ 'response' ][ 'planets' ] ) )
 	{
 		return null;
 	}
-
-	foreach( $Planets[ 'response' ][ 'planets' ] as $Planet )
+	$Planets = $Planets[ 'response' ][ 'planets' ];
+	usort( $Planets, function( $a, $b )
+	{
+		if( $b[ 'state' ][ 'difficulty' ] === $a[ 'state' ][ 'difficulty' ] )
+		{
+			return $a[ 'state' ][ 'current_players' ] - $b[ 'state' ][ 'current_players' ];
+		}
+		
+		return $b[ 'state' ][ 'difficulty' ] - $a[ 'state' ][ 'difficulty' ];
+	} );
+	foreach( $Planets as $Planet )
 	{
 		if( !$Planet[ 'state' ][ 'captured' ]  )
 		{
-			Msg( 'Got planet ' . $Planet[ 'id' ] );
-
+			Msg( 'Got planet ' . $Planet[ 'id' ] . ' with ' . $Planet[ 'state' ][ 'current_players' ] . ' joined players' );
 			return $Planet[ 'id' ];
 		}
 	}
 }
-
 function SendPOST( $Method, $Data )
 {
 	$c = curl_init( );
-
 	curl_setopt_array( $c, [
 		CURLOPT_URL            => 'https://community.steam-api.com/' . $Method . '/v0001/',
 		CURLOPT_USERAGENT      => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3464.0 Safari/537.36',
@@ -158,7 +139,6 @@ function SendPOST( $Method, $Data )
 			'Referer: https://steamcommunity.com/saliengame/play',
 		],
 	] );
-
 	do
 	{
 		Msg( 'Sending ' . $Method . '...' );
@@ -170,16 +150,13 @@ function SendPOST( $Method, $Data )
 		$Data = json_decode( $Data, true );
 	}
 	while( !isset( $Data[ 'response' ] ) );
-
 	curl_close( $c );
 	
 	return $Data;
 }
-
 function SendGET( $Method, $Data )
 {
 	$c = curl_init( );
-
 	curl_setopt_array( $c, [
 		CURLOPT_URL            => 'https://community.steam-api.com/ITerritoryControlMinigameService/' . $Method . '/v0001/?' . $Data,
 		CURLOPT_USERAGENT      => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3464.0 Safari/537.36',
@@ -196,7 +173,6 @@ function SendGET( $Method, $Data )
 			'Referer: https://steamcommunity.com/saliengame/play',
 		],
 	] );
-
 	do
 	{
 		Msg( 'Sending ' . $Method . '...' );
@@ -205,12 +181,10 @@ function SendGET( $Method, $Data )
 		$Data = json_decode( $Data, true );
 	}
 	while( !isset( $Data[ 'response' ] ) );
-
 	curl_close( $c );
 	
 	return $Data;
 }
-
 function Msg( $Message )
 {
 	echo date( DATE_RSS ) . ' - ' . $Message . PHP_EOL;
